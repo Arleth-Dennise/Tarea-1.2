e98cb0d722025833d80def3eb8dba3aad3469b3f
Arleth-Dennise <arleth.oseguera@unah.hn>
2024-07-04 17:05:45 -0600

    app

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/HomePage.dart';
+
+void main() {
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+        
+        bottomSheetTheme: BottomSheetThemeData(
+          backgroundColor: Color.fromARGB(255, 0, 0, 0).withOpacity(0),
+        ),
+      ),
+      routes: {
+        "/": (context) =>  const HomePage(),
+      },
+    );
+  }
+}
